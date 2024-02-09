9ab7b97d260841d4dcbb0a2717e8a8f2fc3db4ff
acando <albert.canadilla@estudiants.urv.cat>
2024-02-09 17:15:25 +0100

    both

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -5,5 +5,5 @@ class Predictor:
 
     def predict_age(min_age, max_age):
         age = random.randint(int(min_age), int(max_age))
-        print("Your age is.. " + str(age) + "!")
+        print("Your age is... " + str(age) + "!")
         return age
