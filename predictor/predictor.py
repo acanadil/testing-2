f219ba1f854ddb9e9a20fd643ccd9ea4503babf2
acando <albert.canadilla@estudiants.urv.cat>
2024-02-09 17:22:57 +0100

    predictor add new function

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -7,3 +7,8 @@ class Predictor:
         age = random.randint(int(min_age), int(max_age))
         print("Your age is.. " + str(age) + "!")
         return age
+    
+    def predict_nationality(nationalities: str[]):
+        index = random.randint(0, len(nationalities))
+        print("Your nationality is.. " + nationalities[index])
+        return nationalities[index]
