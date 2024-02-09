4ae4576883c4d2dbdc12f43cef1dbbdbdbfa4e4e
acando <albert.canadilla@estudiants.urv.cat>
2024-02-09 21:16:40 +0100

    ad

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -5,10 +5,10 @@ class Predictor:
 
     def predict_age(min_age, max_age):
         age = random.randint(int(min_age), int(max_age))
-        print("Your age is.. " + str(age) + "!")
+        print("Your age is... " + str(age) + "!")
         return age
     
     def predict_nationality(nationalities: list[str]):
         index = random.randint(0, len(nationalities)-1)
-        print("Your nationality is.. " + nationalities[index])
+        print("Your nationality is... " + nationalities[index])
         return nationalities[index]
