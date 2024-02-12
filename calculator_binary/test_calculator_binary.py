d591fa1c5e865f82ad5f5ea209f0cd892c4c2662
acando <albert.canadilla@estudiants.urv.cat>
2024-02-12 10:44:36 +0100

    adgsfghdgff

--- calculator_binary/test_calculator_binary.py
+++ calculator_binary/test_calculator_binary.py
@@ -3,7 +3,7 @@ from calculator_binary import Calculator_Binary
 #
 
 def test_add():
-    assert Calculator_Binary.add(1,2) == bin(3.0)
-    assert Calculator_Binary.add(-500, 400) == bin(-100.0)
-    assert Calculator_Binary.add(0, 0) == bin(0.0)
+    assert Calculator_Binary.add(1,2) == bin(3)
+    assert Calculator_Binary.add(-500, 400) == bin(-100)
+    assert Calculator_Binary.add(0, 0) == bin(0)
     
